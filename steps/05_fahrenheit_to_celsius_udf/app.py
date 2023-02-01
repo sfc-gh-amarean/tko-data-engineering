edcc047f4d2ac1973a35197016e8e75e8204b98f
Aria Marean <amarean@dd257f33wt.attlocal.net>
2023-02-01 14:49:56 -0500

    testing github actions

--- steps/05_fahrenheit_to_celsius_udf/app.py
+++ steps/05_fahrenheit_to_celsius_udf/app.py
@@ -11,6 +11,7 @@
 
 import sys
 from scipy.constants import convert_temperature
+
 def main(temp_f: float) -> float:
     return convert_temperature(float(temp_f), 'F', 'C')
    # return (float(temp_f) - 32) * (5/9)
